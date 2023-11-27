4d8620bb2a41efafaa9ddccb175c2a710d5be4ed
Jinho Jang <verycosy@kakao.com>
2023-11-27 12:30:49 +0900

    email confirmation, bacgkround tasks

--- socialapi/tests/routers/test_post.py
+++ socialapi/tests/routers/test_post.py
@@ -59,7 +59,7 @@ async def created_comment(
 # fastapi가 anyio 써서 이미 설치돼있음
 @pytest.mark.anyio
 async def test_create_post(
-    async_client: AsyncClient, registered_user: dict, logged_in_token: str
+    async_client: AsyncClient, confirmed_user: dict, logged_in_token: str
 ):
     body = "Test Post"
 
@@ -74,7 +74,7 @@ async def test_create_post(
     assert {
         "id": 1,
         "body": body,
-        "user_id": registered_user["id"],
+        "user_id": confirmed_user["id"],
     }.items() <= response.json().items()
 
 
@@ -162,7 +162,7 @@ async def test_get_all_posts_wrong_sorting(async_client: AsyncClient):
 async def test_create_comment(
     async_client: AsyncClient,
     created_post: dict,
-    registered_user: dict,
+    confirmed_user: dict,
     logged_in_token: str,
 ):
     body = "Test Comment"
@@ -178,16 +178,16 @@ async def test_create_comment(
         "id": 1,
         "body": body,
         "post_id": created_post["id"],
-        "user_id": registered_user["id"],
+        "user_id": confirmed_user["id"],
     }.items() <= response.json().items()
 
 
 @pytest.mark.anyio
 async def test_create_post_exired_token(
-    async_client: AsyncClient, registered_user: dict, mocker
+    async_client: AsyncClient, confirmed_user: dict, mocker
 ):
     mocker.patch("socialapi.security.access_token_expire_minutes", return_value=-1)
-    token = security.create_access_token(registered_user["email"])
+    token = security.create_access_token(confirmed_user["email"])
     response = await async_client.post(
         "/posts",
         json={"body": "Test Post"},
